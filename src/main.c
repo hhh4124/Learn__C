c35fbbe98b9f1006dafc4d902d184bea411a805e
혀노 <hhh4124@gmail.com>
2021-05-10 14:51:53 +0000

    Using for loop and printing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-//Hello World 출력하기!
+
+// 1. Hello World 출력하기!
 /*
 int main()
 {
@@ -9,7 +10,8 @@ return 0;
 }
 */
 	
-//변수값 할당 해보고 출력해보기
+// 2. 변수값 할당 해보고 출력해보기
+/*
 int main()
 {
 	int num1;
@@ -23,4 +25,21 @@ int main()
 	printf("%d\n%d\n%d\n", num1, num2, num3);
 	
 	return 0;
+}
+*/
+
+// 3. For 반복문 써보고 출력해보기
+int main()
+{
+	int i;
+	int sum = 0;
+	
+	for (i=1; i<=50; i=i+1)
+	{
+		sum = sum + i;
+	}
+	printf("%d\n", sum);
+	
+	return 0;
+	
 }
