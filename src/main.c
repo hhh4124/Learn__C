08029a356e35dcb529d090d63baf699096883994
혀노 <hhh4124@gmail.com>
2021-05-10 14:43:28 +0000

    variables assignment and print

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-
+//Hello World 출력하기!
+/*
 int main()
 {
-	printf("Hello World!\n");
+printf("Hello World!\n");
+
+return 0;
+}
+*/
+	
+//변수값 할당 해보고 출력해보기
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+	
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+	
+	printf("%d\n%d\n%d\n", num1, num2, num3);
 	
 	return 0;
 }
