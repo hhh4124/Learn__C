a5f5d55f2281785b9f4da6a34be609a0a06710ec
혀노 <hhh4124@gmail.com>
2021-07-03 11:13:24 +0000

    Bubble Sort

--- src/main.c
+++ src/main.c
@@ -1067,6 +1067,7 @@ int main()
 */
 
 //포인터 사용해보기
+/*
 int main()
 {
 	int a = 11;
@@ -1076,3 +1077,34 @@ int main()
 	
 	return 0;
 }
+*/
+
+// C언어 정보처리기사 기출문제 풀어보기
+//Bubble Sort
+int main()
+{
+	int i, j;
+	int temp;
+	int a[5] = {75, 95, 85, 100, 50};
+	
+	for(i=0; i<4; i++)
+	{
+		for(j=0; j<4-i; j++)
+		{
+			if(a[j] > a[j+1])
+			{
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+	
+	for(i=0; i<5; i++)
+	{
+	printf("%d\n", a[i]);
+	}
+	
+	return 0;
+}
+
